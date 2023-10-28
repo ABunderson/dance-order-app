55067d89085bb21a623589d6d60f0643faf179f3
Adrienne Bunderson <bun18015@byui.edu>
2023-10-28 10:52:36 -0600

    simplify paths

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import styled from 'styled-components'
-import UserContext from './UserContext';
+import UserContext from 'components/UserContext';
 import { useContext } from 'react'
 
 const StyledHeader = styled.header`
