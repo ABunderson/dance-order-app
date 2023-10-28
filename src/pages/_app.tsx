bac6cf76008e602f82820ae856fa0f70bca136d5
Adrienne Bunderson <bun18015@byui.edu>
2023-10-28 10:38:12 -0600

    try creating context in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,22 @@
 import { useState, createContext } from 'react'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import UserContext from '../../components/UserContext'
-import OrderContext from '../../components/OrderContext'
-import DanceContext from '../../components/DanceContext'
+// import UserContext from '../../components/UserContext'
+// import OrderContext from '../../components/OrderContext'
+// import DanceContext from '../../components/DanceContext'
 
 // export const UserContext = createContext('not set')
+
 
 
 export default function App({ Component, pageProps }: AppProps) {
   const [userName, setUserName] = useState('default')
   const [orderNumber, setOrderNumber] = useState('default')
   const [danceNumber, setDanceNumber] = useState('default')
+
+  const OrderContext = createContext({ orderNumber, setOrderNumber })
+  const DanceContext = createContext({ danceNumber, setDanceNumber })
+  const UserContext = createContext({ userName, setUserName })
 
   return (<>
     <UserContext.Provider value={{ userName, setUserName }}>
